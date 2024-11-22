ed8ad33ad3ffb6e2810d6f2a9406ff8959e0f336
Hudrolax <hudro795@gmail.com>
2024-11-22 09:55:59 -0300

    init

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,7 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- autosave when turn in normal mode
+return {
+  "Pocco81/auto-save.nvim",
+  opts = {},
+}
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,7 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  opts = {
+    priority = 1000,
+    config = true,
+  },
+}
