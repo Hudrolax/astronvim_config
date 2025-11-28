ba120b47b5a6bd3c423d446b38fa10ac5d1a1d63
Sergei Nazarov <hudro@MacBook-Pro-Hudro.local>
2025-11-27 22:09:25 -0300

    sc

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -6,18 +6,12 @@ return {
     vim.g.codeium_disable_bindings = 1
 
     -- Ctrl+y чтобы принять (как в coc.nvim или стандартном nvim)
-    vim.keymap.set("i", "<C-y>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-н>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+    vim.keymap.set("i", "<C-l>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+
     -- листать варианты впред
     vim.keymap.set(
       "i",
       "<C-k>",
-      function() return vim.fn["codeium#CycleCompletions"](1) end,
-      { expr = true, silent = true }
-    )
-    vim.keymap.set(
-      "i",
-      "<C-л>",
       function() return vim.fn["codeium#CycleCompletions"](1) end,
       { expr = true, silent = true }
     )
@@ -29,29 +23,5 @@ return {
       function() return vim.fn["codeium#CycleCompletions"](-1) end,
       { expr = true, silent = true }
     )
-    vim.keymap.set(
-      "i",
-      "<C-о>",
-      function() return vim.fn["codeium#CycleCompletions"](-1) end,
-      { expr = true, silent = true }
-    )
-
-    -- Ctrl+k чтобы принять только слово (частичное принятие)
-    vim.keymap.set(
-      "i",
-      "<C-l>",
-      function() return vim.fn["codeium#AcceptNextWord"]() end,
-      { expr = true, silent = true }
-    )
-    vim.keymap.set(
-      "i",
-      "<C-д>",
-      function() return vim.fn["codeium#AcceptNextWord"]() end,
-      { expr = true, silent = true }
-    )
-
-    -- Принудительный вызов (если вдруг нужно)
-    vim.keymap.set("i", "<C-h>", function() return vim.fn["codeium#Complete"]() end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-р>", function() return vim.fn["codeium#Complete"]() end, { expr = true, silent = true })
   end,
 }
